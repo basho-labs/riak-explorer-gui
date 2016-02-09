eaf1b9c1f34fb39f2c5687ccb078a05fe73492e6
Paul Tannenbaum <paultannenbaum@gmail.com>
2016-02-09 10:52:32 -0800

    Bucket Props Tooltips

---
+++ app/components/tooltip/bucket-props.js
@@ -0,0 +1,38 @@
+import Ember from 'ember';
+import renderTooltip from 'ember-tooltips/utils/render-tooltip';
+import bucketPropsHelp from '../../utils/riak-help-json/bucket_props';
+
+export default Ember.Component.extend({
+  tagName: 'span',
+
+  classNames: ['tooltip-icon', 'bucket-props-tooltip', 'glyphicon', 'glyphicon-info-sign'],
+
+  itemKey: null,
+
+  tooltipInstance: null,
+
+  didRender: function() {
+    let key      = this.get('itemKey');
+    let helpInfo = bucketPropsHelp[key];
+
+    if (helpInfo) {
+      const element = this.$()[0];
+
+      const toolTipTemplate =
+        `
+        <div class='tooltip-content-wrapper'>
+          <div class='title'>${key}</div>
+          <div class='description'>${helpInfo.description}</div>
+          <div class='default'>Default Value: ${helpInfo.default}</div>
+          <div class='editable'>Editable: ${helpInfo.editable}</div>
+          <div class='type'>Type: ${helpInfo.json_schema_type}</div>
+        </div>
+        `;
+
+      this.set('tooltipInstance', renderTooltip(element, {
+        content: toolTipTemplate,
+        event: 'hover'
+      }));
+    }
+  }
+});
