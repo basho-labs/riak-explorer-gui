747e70a872e3542bbb19d597156e61636cc2fd95
Paul Tannenbaum <paultannenbaum@gmail.com>
2016-06-20 11:35:09 -0700

    Present modal dialog to user before performing and Riak Repl actions

--- app/pods/cluster/ops/controller.js
+++ app/pods/cluster/ops/controller.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import Modal from '../../../mixins/controller/modal';
 
-export default Ember.Controller.extend({
+export default Ember.Controller.extend(Modal, {
   availableReplActions: [
     'Cluster Stats',
     'Cluster Manager',
@@ -18,72 +19,22 @@ export default Ember.Controller.extend({
 
   currentReplOutput: '',
 
-  replBaseRoute: '',
-
   actions: {
-    getReplicationOutput: function(action, name) {
-      let slug = '';
-      let url = '';
-      let self = this;
+    cancelReplAction: function() {
+      this.set('currentlySelectedAction', '');
+      this.set('currentReplOutput', '');
+      this.send('hideModal');
+    },
 
+    confirmReplAction: function() {
+      this.send('hideModal');
+      this.send('getReplicationOutput', this.get('currentlySelectedAction'));
+    },
+
+    warnReplAction: function(action) {
       this.set('currentlySelectedAction', action);
       this.set('currentReplOutput', '');
-
-      // Map action to api slug
-      switch(action) {
-        case 'Cluster Stats':
-          slug = 'repl-clusterstats';
-          break;
-        case 'Cluster Manager':
-          slug = 'repl-clusterstats-cluster_mgr';
-          break;
-        case 'Fullsync Start':
-          slug = 'repl-fullsync-start';
-          break;
-        case 'Fullsync Stop':
-          slug = 'repl-fullsync-stop';
-          break;
-        case 'Fullsync Coordinate':
-          slug = 'repl-clusterstats-fs_coordinate';
-          break;
-        case 'Realtime Start':
-          slug = 'repl-realtime-start';
-          break;
-        case 'Realtime Stop':
-          slug = 'repl-realtime-stop';
-          break;
-        case 'Realtime Stats':
-          slug = 'repl-clusterstats-realtime';
-          break;
-        case 'Replication Connections':
-          slug = 'repl-connections';
-          break;
-        case 'Replication Clustername':
-          slug = 'repl-clustername';
-          break;
-        default:
-          break;
-      }
-
-      url = `${this.replBaseRoute}/${slug}`;
-
-      return new Ember.RSVP.Promise(function(resolve, reject) {
-        let request = Ember.$.ajax({
-          url: url,
-          type: 'GET'
-        });
-
-        request.done(function(data) {
-          delete data.links;
-
-          self.set('currentReplOutput', JSON.stringify(data, null, ' '));
-          resolve(data);
-        });
-
-        request.fail(function(data) {
-          reject(data);
-        });
-      });
+      this.send('showModal');
     }
   }
 });
--- app/pods/cluster/ops/route.js
+++ app/pods/cluster/ops/route.js
@@ -1,5 +1,5 @@
+import Ember from 'ember';
 import ClusterRoute from '../route';
-import config from '../../../config/environment';
 
 export default ClusterRoute.extend({
   afterModel: function(model, transition) {
@@ -10,12 +10,68 @@ export default ClusterRoute.extend({
     });
   },
 
-  setupController: function(controller, model) {
-    // Call _super for default behavior
-    this._super(controller, model);
+  actions: {
+    getReplicationOutput: function(action) {
+      let controller = this.controller;
+      let cluster = this.currentModel;
+      let slug;
+      let url;
 
-    let clusterName = model.get('name');
+      // Map action to api slug
+      switch(action) {
+        case 'Cluster Stats':
+          slug = 'repl-clusterstats';
+          break;
+        case 'Cluster Manager':
+          slug = 'repl-clusterstats-cluster_mgr';
+          break;
+        case 'Fullsync Start':
+          slug = 'repl-fullsync-start';
+          break;
+        case 'Fullsync Stop':
+          slug = 'repl-fullsync-stop';
+          break;
+        case 'Fullsync Coordinate':
+          slug = 'repl-clusterstats-fs_coordinate';
+          break;
+        case 'Realtime Start':
+          slug = 'repl-realtime-start';
+          break;
+        case 'Realtime Stop':
+          slug = 'repl-realtime-stop';
+          break;
+        case 'Realtime Stats':
+          slug = 'repl-clusterstats-realtime';
+          break;
+        case 'Replication Connections':
+          slug = 'repl-connections';
+          break;
+        case 'Replication Clustername':
+          slug = 'repl-clustername';
+          break;
+        default:
+          break;
+      }
 
-    controller.set('replBaseRoute', `control/clusters/${clusterName}`);
+      url = `control/clusters/${cluster.get('name')}/${slug}`;
+
+      return new Ember.RSVP.Promise(function(resolve, reject) {
+        let request = Ember.$.ajax({
+          url: url,
+          type: 'GET'
+        });
+
+        request.done(function(data) {
+          delete data.links;
+
+          controller.set('currentReplOutput', JSON.stringify(data, null, ' '));
+          resolve(data);
+        });
+
+        request.fail(function(data) {
+          reject(data);
+        });
+      });
+    }
   }
 });
